f06d5c7659f314906fa827beadb71abf187ef716
ameer017 <rajiabdullahi907@gmail.com>
2024-01-02 16:42:41 +0100

    year_updated

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './Footer.scss'
 
 const Footer = () => {
+  const updatedYear = new Date().getFullYear();
+  
   return (
     <footer className="footer --ta">
-      <p>&copy; 2023, Designed by <a href="mailto: rajiabdullahi907@gmail.com">Raji Abdullah Al Ameer</a>.</p>
+      <p>&copy; {updatedYear} <a href="mailto: rajiabdullahi907@gmail.com">Raji Abdullah Al Ameer</a> | All rights reserved.</p>
     </footer>
   )
 }
