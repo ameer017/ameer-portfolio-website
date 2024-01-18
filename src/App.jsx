f9f86eeb193ff37f49fb43a25b320b4834b621f3
ameer017 <rajiabdullahi907@gmail.com>
2024-01-18 12:00:14 +0100

    what i do?

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Navbar />  
         <HeroSection/>
         <Featured/>
-        {/* <About/> */}
+        <About/>
         {/* <Services/> */}
         {/* <Projects/> */}
         {/* <Contact/> */}
--- src/HeroSection/HeroSection.jsx
+++ src/HeroSection/HeroSection.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import './HeroSection.scss'
-const HeroSection = () => {
+import React from "react";
+import "./HeroSection.scss";
+import {
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiFillGithub,
+  AiFillMediumCircle,
+} from "react-icons/ai";
 
+const HeroSection = () => {
   return (
     <>
-      <section className='heroContainer container'>
-            <div className='hero-text container'>
-              <h1>Abdullah Raji Al Ameer.</h1>
-              <p>I specialize in the artful conception and meticulous construction of outstanding products. By leveraging my skills in design and development, I craft solutions that not only meet but exceed expectations.</p>
-            </div>
+      <section className="heroContainer container">
+        <div className="hero-text container">
+          <h1>Abdullah Raji Al Ameer.</h1>
+          <p>
+            I specialize in the artful conception and meticulous construction of
+            outstanding products. By leveraging my skills in design and
+            development, I craft solutions that not only meet but exceed
+            expectations.
+          </p>
 
-              <img src='/public/IMG_20220319_102711_109~2.jpg' width={250}/>
+          <div className='socials'>
 
-              
+            <a href="https://github.com/ameer017">
+                <AiFillGithub size={18}/>
+            </a>
+            
+            <a href="https://www.instagram.com/a.rajiabdullahi/">
+                <AiOutlineInstagram size={20}/>
+            </a>
+            <a href="https://twitter.com/alAmeer170">
+                <AiOutlineTwitter size={20}/>
+            </a>
+            <a href="https://medium.com/@rajiabdullahi907">
+                <AiFillMediumCircle size={20}/>
+            </a>
+
+          </div>
+        </div>
+
+        <img src="/IMG_20220319_102711_109~2.jpg" width={250} />
       </section>
-
     </>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
