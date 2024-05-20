4a61ef54e29ea31c0221bc7238085a89de4c29e2
ameer017 <rajiabdullahi907@gmail.com>
2024-05-20 11:37:31 +0100

    backend added

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Home Page!")
+})
+
 app.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
   console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
